01507e340f161944b71057150cb50d4f02e27761
shox13 <shox13@gmail.com>
2014-08-27 15:45:00 -0400

    updated
    intergrating pandas library!!!!

--- test_3_csv.py
+++ test_3_csv.py
@@ -1,17 +1,24 @@
 import csv
 #import numpy as np
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from pylab import plotfile, show, gca
 import matplotlib.cbook as cbook
+
+dia = csv.excel()
+
+dia.quoting = csv.QUOTE_NONE
+
+#set dialecting for default csv.
+pd.read_csv(StringIO(data), dialect=dia)
+# read file location and select column for indexing
+pd.read_csv('foo.csv', index_col=[0, 'A'])
+
 
 location=raw_input("What data do you want to process?:")
 fname = cbook.get_sample_data(location, asfileobj=False)
 print "I thought so"
 
 plotfile(fname,(0,8,5,6,8,30))# file name, cycle #, discharge per cycle, Ah, Wh, Charge time per cycle, discharge time from 13.8 to the end.
-fig,ax = plt.subplots()
-ax.plot(dat1['timeDiff'],dat1['Value'])
-ax.plot(dat2['timeDiff'],dat2['Value'])
-plt.show()
+
 
 show()
